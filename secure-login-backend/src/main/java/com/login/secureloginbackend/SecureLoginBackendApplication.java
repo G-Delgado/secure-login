18078e8141a4b0a1c2485a5ea1405219f9b46a99
DaniBonica001 <danielabc_0106@hotmail.com>
2023-11-15 23:30:34 -0500

    Backend base

---
+++ secure-login-backend/src/main/java/com/login/secureloginbackend/SecureLoginBackendApplication.java
@@ -0,0 +1,13 @@
+package com.login.secureloginbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureLoginBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureLoginBackendApplication.class, args);
+	}
+
+}
---
+++ secure-login-backend/src/test/java/com/login/secureloginbackend/SecureLoginBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.login.secureloginbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureLoginBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
