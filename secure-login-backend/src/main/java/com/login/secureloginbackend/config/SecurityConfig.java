9560e157f01d4dc8aa21683c7453fcbf5e31cb79
DaniBonica001 <danielabc_0106@hotmail.com>
2023-11-22 00:22:35 -0500

    Cors config, no test

--- secure-login-backend/src/main/java/com/login/secureloginbackend/config/SecurityConfig.java
+++ secure-login-backend/src/main/java/com/login/secureloginbackend/config/SecurityConfig.java
@@ -1,23 +1,26 @@
 package com.login.secureloginbackend.config;
 
 import com.login.secureloginbackend.config.JWT.JwtAuthFilter;
-import com.login.secureloginbackend.repository.UserRepository;
 import com.login.secureloginbackend.service.security.UserManagementService;
-import lombok.AllArgsConstructor;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.authorization.AuthorityAuthorizationManager;
+import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
+import org.springframework.security.web.access.intercept.RequestMatcherDelegatingAuthorizationManager;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.security.web.util.matcher.AndRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -27,16 +30,28 @@ public class SecurityConfig {
     private final AuthenticationProvider authenticationManager;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthorizationManager<RequestAuthorizationContext> access) throws Exception{
         return http
-                .authorizeRequests()
-                .anyRequest()
-                .authenticated()
-                .and()
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.anyRequest().access(access))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationManager)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
+    @Bean
+    public AuthorizationManager<RequestAuthorizationContext> requestMatcherAuthenticationManager(HandlerMappingIntrospector introspector){
+        RequestMatcher permitAll = new AndRequestMatcher(new MvcRequestMatcher(introspector,"/auth/login"),
+                new MvcRequestMatcher(introspector,"/auth/signup"), new MvcRequestMatcher(introspector,"/h2-console"));
+
+        RequestMatcherDelegatingAuthorizationManager.Builder managerBuilder
+                = RequestMatcherDelegatingAuthorizationManager.builder()
+                .add(permitAll,(context,other)-> new AuthorizationDecision(true));
+
+        AuthorizationManager<HttpServletRequest> manager = managerBuilder.build();
+        return (authentication, object) -> manager.check(authentication,object.getRequest());
+
+    }
 }
